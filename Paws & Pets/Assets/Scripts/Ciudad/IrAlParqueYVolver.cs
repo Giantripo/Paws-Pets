c4a59b1ddb4baeb4c64c04c4c9f41ec83b32f057
Gian Franco <giancapo2011@gmail.com>
2022-06-22 22:45:18 -0300

    primer cambio de movimiento en la ciudad

---
+++ Paws & Pets/Assets/Scripts/Ciudad/IrAlParqueYVolver.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IrAlParqueYVolver : MonoBehaviour
+{
+    public GameObject CiudadBackGround;
+    public GameObject Flecha;
+    //public GameObject Jardin;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (CiudadBackGround.transform.position.x == -22.53f)
+        {
+            CiudadBackGround.transform.position = new Vector2(0.01f, 0);
+            Flecha.transform.position = new Vector2(9.99f, -1.78f);
+        }
+        else
+        {
+            CiudadBackGround.transform.position = new Vector2(-22.53f, 0);
+            Flecha.transform.position = new Vector2(-9.83f, -1.78f);
+            
+        }
+        
+    }
+
+}
