c357040c3137f16b964484a56b4bbd63e3cf94c7
Gian Franco <giancapo2011@gmail.com>
2022-06-30 23:09:26 -0300

    reptario y cangrejologo en el shelter

---
+++ Paws & Pets/Assets/FlechaZonaCangrejos.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlechaZonaCangrejos : MonoBehaviour
+{
+    public GameObject CiudadBackGround;
+    public GameObject Flecha;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnMouseDown()
+    {
+        if (CiudadBackGround.transform.position.x == 46.48f)
+        {
+            CiudadBackGround.transform.position = new Vector2(68.48f, 0);
+            Flecha.transform.position = new Vector2(9.5f, -1.78f);
+            //OtraFlecha.transform.position = new Vector2(9.8f, -1.78f);
+            Flecha.transform.Rotate(new Vector3(0, -180, 0));
+        }
+        else
+        {
+            CiudadBackGround.transform.position = new Vector2(46.48f, 0);
+            Flecha.transform.position = new Vector2(-9.5f, -1.99f);
+            //OtraFlecha.transform.position = new Vector2(9.8f, -80.78f);
+            Flecha.transform.Rotate(new Vector3(0, 180, 0));
+
+        }
+
+    }
+}
